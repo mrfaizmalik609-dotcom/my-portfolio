cc316c4a54460a3985ce656eee3191846e3b031a
Faiz Malik <mrfaizmalik609@gmail.com>
2025-08-29 06:18:06 +0500

    ok

--- my/src/vite.config.js
+++ my/src/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',    // Vercel deploy ke liye
-})
+  root: ".",  // root folder
+  base: "/",  // important for deploy
+});
